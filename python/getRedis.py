7288495ef49b2ac5063dcaac030db4b1e9c3bb9b
jiafeng <jiafeng@enlink.cn>
2023-08-09 17:40:30 +0800

    新增getRedis.py

---
+++ python/getRedis.py
@@ -0,0 +1,45 @@
+import redis
+from openpyxl import Workbook
+
+def get_redis_data(workbook, r, key):
+    data = None
+    count = 1
+    
+    sheet_name = key.split(":")[-1]
+    print(sheet_name)
+    sheet = workbook.create_sheet(title=sheet_name)
+    
+    typ = r.type(key).decode('utf-8')
+    if typ == "hash":
+        data = r.hgetall(key)  # 替换为你的Redis key
+        for key, value in data.items():
+            sheet['A'+str(count)] = key
+            sheet['B'+str(count)] = value
+            count += 1
+    elif typ == "set":
+        data = r.smembers(key)
+        for value in data:
+            sheet['A'+str(count)] = value
+            count += 1
+
+# Redis连接信息
+redis_host = 'localhost'
+redis_port = 6378
+redis_password = 'enlink'  # 如果没有设置密码，可以将此行注释掉
+
+# Excel文件路径
+excel_file = 'redis_data.xlsx'
+
+# 连接Redis
+r = redis.Redis(host=redis_host, port=redis_port, password=redis_password)
+
+workbook = Workbook()
+sheet = workbook.active
+workbook.remove(sheet)
+keys = r.keys(pattern="terminal:deviceProgramMp:*")
+for key in keys:
+    get_redis_data(workbook, r, key.decode('utf-8'))
+
+# 保存Excel文件
+workbook.save(excel_file)
+print(f"Redis数据已成功写入Excel文件：{excel_file}")
