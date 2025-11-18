7375504d66ae3c5a7b72e31a5bfb009bbd8f691f
jiafeng <jiafeng@enlink.cn>
2025-11-18 08:45:35 +0800

    优化upload

--- python/upload/upload.py
+++ python/upload/upload.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, File, Request, UploadFile
-from fastapi.templating import Jinja2Templates
 import os
 import shutil
+from fastapi import FastAPI, File, Request, UploadFile
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse, JSONResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -21,6 +22,15 @@ async def upload(file: UploadFile = File(...)):
         shutil.copyfileobj(file.file, buffer)
 
     return {"filename": filename, "message": "文件上传成功"}
+
+@app.get("/redirect")
+async def redirect():
+    return RedirectResponse(url="https://wd.enlink.top")
+
+@app.post("/post-data")
+async def post_data(data: dict):
+    # 这里我们简单返回接收到的数据
+    return JSONResponse(data)
 
 if __name__ == "__main__":
     import uvicorn
